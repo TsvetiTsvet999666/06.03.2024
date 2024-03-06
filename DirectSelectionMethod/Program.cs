19f2472c8a186c70f61580d2621642a43bde370f
TsvetiTsvet999666 <159997613+TsvetiTsvet999666@users.noreply.github.com>
2024-03-06 17:20:27 +0200

    06.03.2024

---
+++ DirectSelectionMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DirectSelectionMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 2, 4, -5, 1, 10 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j]) 
+                    {
+                        int swapVar = array[i];
+                        array[i] = array[j];
+                        array[j] = swapVar;
+                    }
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace InsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 4, -5, 1, 10 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int swapVar = arr[i];
+                int index = i;
+                while (index > 0 && arr[index - 1] >= swapVar)
+                {
+                    arr[index] = arr[index - 1];
+                    index--;
+                }
+                arr[index] = swapVar;
+            }
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
+
